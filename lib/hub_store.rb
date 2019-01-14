b7e99efb88543e600dc42d440d1dd9cd8416ecfd
Jens Balvig <jens@balvig.com>
2019-01-14 10:58:55 +0900

    import more stuff

--- lib/hub_store.rb
+++ lib/hub_store.rb
@@ -6,7 +6,7 @@ require "hub_store/exporter"
 module HubStore
   class Error < StandardError; end
 
-	# Set up a database that resides in RAM
+	# Set up database
 	ActiveRecord::Base.establish_connection(
 		adapter: "sqlite3",
 		database: "db/hub_store"
@@ -17,7 +17,15 @@ module HubStore
     unless ActiveRecord::Base.connection.table_exists?("pull_requests")
       create_table "pull_requests", force: :cascade do |t|
         t.string "submitter"
+        t.string "number"
         t.string "labels"
+        t.string "repo"
+        t.integer "approval_time"
+        t.integer "time_to_first_review"
+        t.integer "merge_time"
+        t.integer "additions"
+        t.integer "review_count"
+        t.boolean "straight_approval"
         t.datetime "closed_at"
         t.datetime "created_at", null: false
         t.datetime "updated_at", null: false
